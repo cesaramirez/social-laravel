6851a40e767674e364ce3e6ebbbc75834b993d2e
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-10-14 09:12:41 -0600

    Add dependencies

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,33 +1,15 @@
+import lodash from 'lodash';
+import jquery from 'jquery';
+import axios from 'axios';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
-window._ = require('lodash');
+UIkit.use(Icons);
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
+window._ = lodash;
+window.$ = window.jQuery = jquery;
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
@@ -36,18 +18,3 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
