f562f5106a7d324f93470537ad36e2f92d8db19a
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-10-19 22:14:23 -0600

    Change .env.example and add browsersync env variable

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+require('dotenv').config();
 
 /*
  |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.browserSync({
-       proxy: 'social.laravel.dev'
-   })
+mix.browserSync(process.env.APP_URL);
