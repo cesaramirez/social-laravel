21df763d826f04e2c2df60236625fd463e3717ac
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-09 10:46:41 -0600

    Upgrade UIKit :arrow_up: and remove jQuery :tada:

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,5 +1,4 @@
 import lodash from 'lodash';
-import jquery from 'jquery';
 import axios from 'axios';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
@@ -7,14 +6,13 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 window._ = lodash;
-window.$ = window.jQuery = jquery;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
