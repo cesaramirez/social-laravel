d56cb1a8e7d10c77b9082d7302a2f63ecbfa3419
César Ramírez <cesar.ramirez.sv@gmail.com>
2018-06-17 20:08:45 -0600

    :package: Update to Laravel 5.6 and latest UIkit version

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,18 +1,6 @@
-import lodash from 'lodash';
-import axios from 'axios';
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
 
 UIkit.use(Icons);
 
-window._ = lodash;
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
